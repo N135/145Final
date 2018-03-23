cc66bb968862a274a52da9aea219f26682305a87
N135 <nawhite@ucdavis.edu>
2018-03-22 20:28:50 -0700

    Fixed it!

--- MachRun3.r
+++ MachRun3.r
@@ -26,15 +26,20 @@ while(isActive()){
 	#Decrease number of machines up
 	Shared[1,2] <- Shared[1,2] - 1
 
+	if (Shared[1,2] == 2){
+		Shared[1,2] <- 1
+	}
+	if (Shared[1,2] == -1){
+		Shared[1,2] <- 0
+	}
+
 	#If one is still active, passivate this machine and wait for the other to go down
 	if (Shared[1,2] == 1){
-		print(paste("Passivating",self[1], Shared[1,2]))
 		passivate(self)
 	}
 	#if both are down, reactivate the other machine so both can call for repairs
 	else{
 		if(checkAvail(1)){
-			print(paste("Reactivating:", self[1] %% 2 + 1))
 			#get other machine id
 			reactivate(self[1] %% 2 + 1)
 		}
@@ -47,8 +52,8 @@ while(isActive()){
 
 	hold(self, RepTime)
 
-	Shared[1,2] <- Shared[1,2] + 1
 
+	Shared[1,2] <- Shared[1,2] + 1
 
 	release(self,1)
 }
